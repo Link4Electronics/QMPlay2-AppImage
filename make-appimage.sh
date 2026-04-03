7eb2e733fb474c6ca18ada324a8c9d6e6a61de0e
Link4Electronics <link4electronics@gmail.com>
2026-04-03 02:56:02 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/256x256/apps/QMPlay2.png
 export DESKTOP=/usr/share/applications/QMPlay2.desktop
 export DEPLOY_QT=1
+export QT_DIR=qt6
 export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
 
