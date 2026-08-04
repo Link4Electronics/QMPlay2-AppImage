32f665b1686a38f0de9e46e406add5aa328d027e
Link4Electronics <link4electronics@gmail.com>
2026-08-04 00:19:58 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,6 +19,6 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
-make-aur-package "qmplay2-git"
+make-aur-package qmplay2-git
 
 # If the application needs to be manually built that has to be done down here
